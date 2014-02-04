0ef480582c8d6b35ef9f4037d781b57d8c45cb85
GIT Admin <houngj@students.wwu.edu>
2014-02-04 02:36:36 -0800

    Finished RingSolution

--- RingSolution.c
+++ RingSolution.c
@@ -2,10 +2,11 @@
 #include <string.h>
 #include <mpi.h>
 #include <stdlib.h>
+#include <unistd.h>
 const int MAX_STRING = 100;
 
 int main(int argc, char *argv[]){
-  unsigned int seed = 10;
+  
   char message[2];
   int comm_sz;
   int my_rank;
@@ -14,15 +15,68 @@ int main(int argc, char *argv[]){
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-  int A[comm_sz];
+  unsigned int seed = my_rank;
   
-  int i = 0;
-  for(i = 0; i < comm_sz; i++){
-    A[i] = rand_r(&seed);
+  int randomnum = rand_r(&seed) % 100;
+  min = randomnum;
+  max = min;
+  sleep(rand_r(&seed) % 10);
+  //first phase
+  
+  
+  if(my_rank == 0){
+    sprintf(message, "%d", min);
+    MPI_Send(message, strlen(message)+1, MPI_CHAR, my_rank+1, 0, MPI_COMM_WORLD);
+    sprintf(message, "%d", max);
+    MPI_Send(message, strlen(message)+1, MPI_CHAR, my_rank+1, 0, MPI_COMM_WORLD);
+    MPI_Recv(message, MAX_STRING, MPI_CHAR, comm_sz-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    min = atoi(message);
+    MPI_Recv(message, MAX_STRING, MPI_CHAR, comm_sz-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    max = atoi(message);
+    printf("--------rank-%02d-------- min - %d max - %d\n", my_rank, min, max);
+  } else {
+    MPI_Recv(message, MAX_STRING, MPI_CHAR, my_rank-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    min = atoi(message);
+    MPI_Recv(message, MAX_STRING, MPI_CHAR, my_rank-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    max = atoi(message);
+    if(randomnum < min){
+      sprintf(message, "%d", randomnum);
+    }
+    else{
+      sprintf(message, "%d", min);
+    }
+    MPI_Send(message, strlen(message)+1, MPI_CHAR, (my_rank+1) % comm_sz, 0, MPI_COMM_WORLD);
+    if(randomnum > max){
+      sprintf(message, "%d", randomnum);
+    }
+    else {
+      sprintf(message, "%d", max);
+    }
+    MPI_Send(message, strlen(message)+1, MPI_CHAR, (my_rank+1) % comm_sz, 0, MPI_COMM_WORLD);
+    
+    
   }
 
+  //second phase
   
-
-
-
+  if(my_rank == 0){
+    sprintf(message, "%d", min);
+    MPI_Send(message, strlen(message)+1, MPI_CHAR, my_rank+1, 0, MPI_COMM_WORLD);
+    sprintf(message, "%d", max);
+    MPI_Send(message, strlen(message)+1, MPI_CHAR, my_rank+1, 0, MPI_COMM_WORLD);
+  }
+  else{
+    MPI_Recv(message, MAX_STRING, MPI_CHAR, my_rank-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    min = atoi(message);
+    MPI_Recv(message, MAX_STRING, MPI_CHAR, my_rank-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    max = atoi(message);
+    printf("--------rank-%02d-------- min - %d max - %d\n", my_rank, min, max);
+    sprintf(message, "%d", min);
+    MPI_Send(message, strlen(message)+1, MPI_CHAR, (my_rank+1) % comm_sz, 0, MPI_COMM_WORLD);
+    sprintf(message, "%d", max);
+    MPI_Send(message, strlen(message)+1, MPI_CHAR, (my_rank+1) % comm_sz, 0, MPI_COMM_WORLD);
+  }
+  
+  MPI_Finalize();
+  return 0;
 }
