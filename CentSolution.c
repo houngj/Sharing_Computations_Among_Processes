74041c512130f2bc5a814bf311cf077ade8ba89a
GIT Admin <houngj@students.wwu.edu>
2014-02-04 20:23:02 -0800

    fixed output with run time

--- CentSolution.c
+++ CentSolution.c
@@ -4,68 +4,65 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
-const int MAX_STRING = 100;
+#include <time.h>
+
 int main(int argc, char * argv[]){
   
-  
-  char message[MAX_STRING];
+  int message;
   int comm_sz; //number of processes
   int my_rank; //my process rank
-  int min = 100;
-  int max = 0;
+  int min;
+  int max;
+  
+  
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   unsigned int seed = my_rank;
-  /*int A[comm_sz];
-  int i = 0;
-  for (i = 0; i < comm_sz; i++){
-    A[i] = rand_r(&seed);
-    
-    }*/
-  
+  double local_start, local_finish, local_elapsed, elapsed;
+  MPI_Barrier(MPI_COMM_WORLD);
+  local_start = MPI_Wtime();
   if(my_rank != 0){
-    sprintf(message, "%d", rand_r(&seed) % 100);
-    
-    sleep(rand_r(&seed) % 10);
-    
-    MPI_Send(message, strlen(message)+1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
-    MPI_Recv(message, MAX_STRING, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    min = atoi(message);
-    MPI_Recv(message, MAX_STRING, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    max = atoi(message);
-    printf("----process-%02d----min - %d max - %d\n",my_rank, min, max);
+    message = rand_r(&seed);
+    usleep(rand_r(&seed)%1000);
+    MPI_Send(&message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    MPI_Recv(&message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    min = message;
+    MPI_Recv(&message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    max = message;
+    //printf("----process-%02d----min - %d max - %d\n",my_rank, min, max);
   }
   else {
-    //printf("%d\n", A[my_rank] % 100);
-    min = rand_r(&seed) % 100;
-    max = rand_r(&seed) % 100;
     
-    sleep(rand_r(&seed) % 10);
+    min = rand_r(&seed);
+    max = rand_r(&seed);
+    usleep(rand_r(&seed)%1000);
+    
     int q = 1;
     for(q = 1; q < comm_sz; q++){
-      MPI_Recv(message, MAX_STRING, MPI_CHAR, q, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      int current = atoi(message);
+      MPI_Recv(&message, 1, MPI_INT, q, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      int current = message;
       if (current < min){
 	min = current;
       } 
       if (current > max){
 	max = current;
       }
-      
     }
-    printf("----process-%02d---- min - %d max - %d\n", my_rank, min, max);
+    //printf("----process-%02d---- min - %d max - %d\n", my_rank, min, max);
     for(q = 1; q < comm_sz; q++){
-      sprintf(message, "%d", min);
-      MPI_Send(message, strlen(message)+1, MPI_CHAR, q, 0, MPI_COMM_WORLD);
-      sprintf(message, "%d", max);
-      MPI_Send(message, strlen(message)+1, MPI_CHAR, q, 0, MPI_COMM_WORLD);
-    }
-    
+      message = min;
+      MPI_Send(&message, 1, MPI_INT, q, 0, MPI_COMM_WORLD);
+      message = max;
+      MPI_Send(&message, 1, MPI_INT, q, 0, MPI_COMM_WORLD);
+    }  
   }
-
-
+  local_finish = MPI_Wtime();
+  local_elapsed = local_finish - local_start;
+  MPI_Reduce(&local_elapsed, &elapsed, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
   MPI_Finalize();
-
+  if (my_rank == 0)
+    printf("%f\n", elapsed);
+  
   return 0;
 }
--- SymmSolution.c
+++ SymmSolution.c
@@ -6,7 +6,7 @@
 const int MAX_STRING = 100;
 int main(int argc, char * argv[]){
   
-  char message[2];
+  int message;
   int comm_sz;
   int my_rank;
   int min;
@@ -16,25 +16,25 @@ int main(int argc, char * argv[]){
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   
   unsigned int seed = my_rank;
+  double local_start, local_finish, local_elapsed, elapsed;
+  MPI_Barrier(MPI_COMM_WORLD);
   int randnum = rand_r(&seed); 
-  
-  min = randnum%100;
+  local_start = MPI_Wtime();
+  min = randnum;
   
   max = min;
-  sprintf(message, "%d", min);
-  sleep(randnum%10);
+  message = min;
+  usleep(randnum%1000);
   int i = 0;
   for(i = 0; i < comm_sz; i++){
     if(i != my_rank){
-      
-      MPI_Send(message, strlen(message)+1, MPI_CHAR, i, 0, MPI_COMM_WORLD);
+      MPI_Send(&message, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
     }
   }
-  
   for(i = 0; i < comm_sz; i++){
     if(i != my_rank){
-      MPI_Recv(message, MAX_STRING, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      int current = atoi(message);
+      MPI_Recv(&message, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      int current = message;
       if(current < min){
 	min = current;
       }
@@ -44,8 +44,13 @@ int main(int argc, char * argv[]){
     }
   }
   
-  printf("--------rank-%02d-------- min - %d max - %d\n", my_rank, min, max);
+  //printf("--------rank-%02d-------- min - %d max - %d\n", my_rank, min, max);
+  local_finish = MPI_Wtime();
+  local_elapsed = local_finish - local_start;
+  MPI_Reduce(&local_elapsed, &elapsed, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
   MPI_Finalize();
+  if (my_rank == 0)
+    printf("%f\n", elapsed);
   return 0;
 
   
